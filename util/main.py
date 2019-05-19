0d160e1960600ef2b3f590432d779e4a810ceb48
rakeshvarma96 <rakeshvarma8106513311@gmail.com>
2019-05-19 23:03:06 +0530

    Adding Exception Handling

--- util/main.py
+++ util/main.py
@@ -32,64 +32,67 @@ def get_total_requests(soup):
 
 @app.route("/issues")
 def get_issues():
-    URL = request.args['GitURL'] + '/pulls'
-    r = requests.get(URL)
+    try:
+        URL = request.args['GitURL'] + '/pulls'
+        r = requests.get(URL)
 
-    soup = BeautifulSoup(r.content, 'html5lib')
+        soup = BeautifulSoup(r.content, 'html5lib')
 
-    one_day_old = 0
-    seven_days_old = 0
-    total_requests = get_total_requests(soup)
+        one_day_old = 0
+        seven_days_old = 0
+        total_requests = get_total_requests(soup)
 
-    count = 0
-    for x in soup.findAll('div', attrs={'class': 'pagination'}):
-        for y in x.findAll('em'):
-            count = int(y['data-total-pages'])
-            break
-        gen_url = ''
-        for w in x.findAll('a'):
-            gen_url = w['href']
-            break
-        gen_url = gen_url.split('&')
-        first = gen_url[0].split('=')[0] + '='
-        for z in range(1, count + 1):
-            page = 'http://github.com' + first + str(z) + '&' + gen_url[1]
-            r = requests.get(page)
-            mysoup = BeautifulSoup(r.content, 'html5lib')
-            done = False
-            for a in mysoup.findAll('span', attrs={'class': 'opened-by'}):
-                for b in a.findAll('relative-time'):
-                    d = dateutil.parser.parse(b['datetime'])
-                    d = d.replace(tzinfo=None)
-                    days_old = prettydate(d)
-                    if days_old == 1:
-                        one_day_old += 1
-                    elif days_old == 2:
-                        seven_days_old += 1
-                    else:
-                        done = True
+        count = 0
+        for x in soup.findAll('div', attrs={'class': 'pagination'}):
+            for y in x.findAll('em'):
+                count = int(y['data-total-pages'])
+                break
+            gen_url = ''
+            for w in x.findAll('a'):
+                gen_url = w['href']
+                break
+            gen_url = gen_url.split('&')
+            first = gen_url[0].split('=')[0] + '='
+            for z in range(1, count + 1):
+                page = 'http://github.com' + first + str(z) + '&' + gen_url[1]
+                r = requests.get(page)
+                mysoup = BeautifulSoup(r.content, 'html5lib')
+                done = False
+                for a in mysoup.findAll('span', attrs={'class': 'opened-by'}):
+                    for b in a.findAll('relative-time'):
+                        d = dateutil.parser.parse(b['datetime'])
+                        d = d.replace(tzinfo=None)
+                        days_old = prettydate(d)
+                        if days_old == 1:
+                            one_day_old += 1
+                        elif days_old == 2:
+                            seven_days_old += 1
+                        else:
+                            done = True
+                            break
+                    if done:
                         break
+                        # print(b['datetime'])
                 if done:
                     break
-                    # print(b['datetime'])
-            if done:
-                break
 
-    output = '''<table border="1">
-  <tr>
-    <th>Less than 24 Hours old</th>
-    <th>Less than 7 days and more than 24 hours old</th>
-    <th>More than 7 days old</th>
-    <th>Total Open Issues</th>
-  </tr>
-  <tr>
-    <td>{0}</td>
-    <td>{1}</td>
-    <td>{2}</td>
-    <td>{3}</td>
-  </tr>
-</table>'''.format(one_day_old, seven_days_old, total_requests - (one_day_old + seven_days_old), total_requests)
-    return output
+        output = '''<table border="1">
+      <tr>
+        <th>Less than 24 Hours old</th>
+        <th>Less than 7 days and more than 24 hours old</th>
+        <th>More than 7 days old</th>
+        <th>Total Open Issues</th>
+      </tr>
+      <tr>
+        <td>{0}</td>
+        <td>{1}</td>
+        <td>{2}</td>
+        <td>{3}</td>
+      </tr>
+    </table>'''.format(one_day_old, seven_days_old, total_requests - (one_day_old + seven_days_old), total_requests)
+        return output
+    except Exception:
+        return 'Invalid GitHub Repository URL'
 
 
 @app.route("/")
